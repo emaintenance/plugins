394b3cfd9a18e513e501786185ac1ba59ef31b80
emaintenance <Matthieu-externe.Perrin@edf.fr>
2016-06-09 13:20:20 +0200

    This script convert an active check to passive, without changing the plugin.

---
+++ active2passive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# THIS PLUGIN IS PROVIDED "AS IS", WITHOUT ANY WARRANTY, AND WITHOUT ANY SUPPORT. 
+# Matthieu PERRIN https://fr.linkedin.com/in/perrinmatthieu - 2014
+
+
+if [ -z "$3" ]
+then
+    echo "Usage : $0 plugin hostname service 'arguments1 arguments2'"
+    exit 3
+fi
+
+PATH=$PATH:/usr/local/nagios/libexec
+
+check=$1
+hostname=$2
+service=$3
+arg1=$4
+
+res=3
+
+text=$($check $arg1)
+res=$(echo $?)
+
+echo "$hostname;$service;$res;$text"
